083ef61f1934fdfeac6550a33d6a705e9e084db5
Artur Folwaczny <afolwaczny9@gmail.com>
2026-01-19 00:30:13 +0100

    dodanie klasy Worker i Ramp

--- include/nodes.hxx
+++ include/nodes.hxx
@@ -1,17 +1,20 @@
 #include "types.hxx"
 #include "package.hxx"
 #include <optional>
+#include <memory>
+
 class RecivePreferences{
 
 };
 
-class PackegeSender{
+class PackageSender{
 public:
-    PackegeSender(PackegeSender&&)=default; //konstruktor 
+    PackageSender()=default;
+    PackageSender(PackageSender&&)=default;
 
-    RecivePreferences receiver_preferences_; //pobranie preferencji z klasy RecivePreferences
+    RecivePreferences receiver_preferences_; 
 
-    void send_package(); //wymuszenie metody na klasach Ramp Worker
+    void send_package(); 
 
     std::optional<Package>& get_sending_buffer() {
         return buffer_;
@@ -26,6 +29,52 @@ protected:
     std::optional<Package> buffer_ = std::nullopt;
 };
 
+class Ramp: public PackageSender{
+public:
+    Ramp(ElementID id,TimeOffset di){
+        id_=id;
+        di_=di;
+    }
+    void delivergoods(Time t);
 
+    TimeOffset get_delivery_interval() const{
+        return di_;
+    };
+    ElementID get_id(){
+        return id_;
+    }
+
+
+private:
+    ElementID id_;
+    TimeOffset di_;
+    Time t_;
+
+};
+
+class Worker: public PackageSender{
+public:
+    Worker(ElementID id,TimeOffset pd, std::unique_ptr<IPackageQueue> q){
+        id_=id;
+        pd_=pd;
+        q_=std::move(q);
+    }
+
+    void do_work(Time t);
+    
+    TimeOffset get_processing_duration(){return pd_;};
+
+    Time get_processing_duration(){return t_;}; //o co tu chodzi?
+
+
+
+private:
+   ElementID id_;
+   TimeOffset pd_;
+   std::unique_ptr<IPackageQueue> q_;
+   Time t_;
+
+
+};
 
 
--- include/types.hxx
+++ include/types.hxx
@@ -1,3 +1,5 @@
 //ty maja być te typy dancyh 
 //using <nazwa nasza>=tyo
 using ElementID = int;
+using Time=unsigned; 
+using TimeOffset=unsigned; 
