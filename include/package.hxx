e65f41f070b2652cee485aa7a1de33a4598dc094
PiotrGalazka <piotrgalazka@student.agh.edu.pl>
2026-01-18 21:13:21 +0100

    utworzenie klasy Package

--- include/package.hxx
+++ include/package.hxx
@@ -1,5 +1,19 @@
 #include "types.hxx"
+#include <set>
 
 class Package {
-    
+public:
+    Package();
+    Package(ElementID ID) : ID_(ID){};
+    Package(Package&& package) : ID_(package.ID_) {};
+
+    Package& operator=(Package&& package);
+    ElementID get_id() const {return ID_;}
+
+    ~Package();
+
+private:
+    ElementID ID_;
+    std::set<ElementID> used_ID;
+    std::set<ElementID> freed_ID;
 };
